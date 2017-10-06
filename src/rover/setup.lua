3816d26e6ba8676b52abc4c5a068fd67aaab725b
Michal Cichra <michal@cichra.cz>
2017-10-06 11:06:33 +0200

    setup only when rockfile is available

--- src/rover/setup.lua
+++ src/rover/setup.lua
@@ -1,11 +1,14 @@
 local _M = {
-    env = require('rover.env')
+    env = require('rover.env'),
+    roverfile = require('rover.roverfile').read()
 }
 
 local mt = {}
 
 function mt.__call()
-    package.path = _M.env.path()
+    if _M.roverfile then
+        package.path = _M.env.path()
+    end
 end
 
 return setmetatable(_M, mt)()
